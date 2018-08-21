a0fc9ecb975ecce03ce5fbeded2bd3e9844d63fe
Laurel-rao <42195541+Laurel-rao@users.noreply.github.com>
2018-08-21 08:45:09 +0800

    Update attr.py

--- day01/attr.py
+++ day01/attr.py
@@ -8,15 +8,24 @@ a = np.array([
     [7 + 3j, 8 + 6j, 9 + 9j],
 ])
 # b = a.shape()
-
+# 数组属性
 print(a.dtype, a.dtype.str, a.dtype.char)
+# 数组形状（各维度值，从大到小）
 print(a.shape)
+# 维度数量
 print(a.ndim)
+# 大小，元素的数量
 print(a.size, len(a))
+# 数组中元素的大小，以字节为单位
 print(a.itemsize)
+# 数组总体的大小，以字节为单位
 print(a.nbytes)
+# 数据进行转换后，生成一个新的形状的数组，按照线性代数进行转换
 print(a.T)
+# 复数的实部
 print(a.real)
+# 复数的虚部
 print(a.imag)
+# 扁平迭代器
 for item in a.flat:
     print(item)
